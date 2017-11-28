9c871ab3ceb63f3c300dbd07a235591c86ebf761
Michael Mosquera <shepherd777@gmail.com>
2017-11-28 04:56:24 -0500

    testing

--- app/src/main/java/group19/employeetracker/GroupActivity.java
+++ app/src/main/java/group19/employeetracker/GroupActivity.java
@@ -50,9 +50,9 @@ public class GroupActivity extends AppCompatActivity
 
             Log.d(LOG_TAG, "Will eventually launch new activity!");
 
-            String groupId = clickedGroupListItem.getId();
-
             int groupId = clickedGroupListItem.getId();
+
+            //int groupId = clickedGroupListItem.getId();
 
             // Create intent and switch to map activity
         });
